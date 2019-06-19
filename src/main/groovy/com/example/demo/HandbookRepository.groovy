4e1de5a538041e0a4942eaf9b91007cfa697ac3e
dumoff77 <sirenchenkod@gmail.com>
2019-06-19 20:54:54 +0200

    CRUD API

--- src/main/groovy/com/example/demo/HandbookRepository.groovy
+++ src/main/groovy/com/example/demo/HandbookRepository.groovy
@@ -3,10 +3,13 @@ package com.example.demo
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.Query
 
+import javax.swing.text.Document
+
 interface HandbookRepository extends MongoRepository<Handbook, String>{
 
     Handbook findByName(String name)
     Handbook removeByName(String name)
+
 
 
 }
--- src/main/groovy/com/example/demo/HomeController.groovy
+++ src/main/groovy/com/example/demo/HomeController.groovy
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+//import groovy.json.JsonSlurper
 
+@Slf4j
 @Controller
 class HomeController {
 
@@ -31,11 +35,6 @@ class HomeController {
     //    'ok'
     //}
     //
-    //@GetMapping('/handbook')
-    //def handbook(Model model) {
-    //    handbookRepository.findAll().toString()
-    //}
-
     //@GetMapping('/add')
     //def add() {
     //    userRepository.save(new User(
@@ -44,28 +43,59 @@ class HomeController {
     //    ))
     //}
 
+    @GetMapping('api/handbook')
+    def handbook(Model model) {
+        handbookRepository.findAll().toString()
+    }
 
-    @DeleteMapping('/delete/{name}')
+    @DeleteMapping('api/handbook/{name}')
     def delete(@PathVariable String name) {
         try {
-            println name
+            log.debug("Start delete handbook by name $name")
             handbookRepository.removeByName(name)
             return 'ok'
         } catch (Exception e) {
-            return "ErrorPage"
+            log.error(e.message, e)
         }
+        log.info("Deleted handbook with name $name")
     }
 
-    @PostMapping("/insert")
-    def insert(@RequestParam String number, String name, String description) {
-        println 'POST'
+    //@PostMapping("api/handbook")
+    //def insert(@RequestParam String number, String name, String description) {
+    //    try {
+    //        handbookRepository.save(new Handbook(
+    //                name: name,
+    //                description: description,
+    //                number: number))
+    //        return 'ok'
+    //    } catch (Exception e) {
+    //        return "ErrorPage"
+    //    }
+    //}
+
+    @PostMapping("api/handbook")
+    def insert(@RequestBody Handbook handbook) {
         try {
-            handbookRepository.save(new Handbook(
-                    name: name,
-                    description: description,
-                    number: number))
-        } catch (Exception e) {
-            return "ErrorPage"
+            handbookRepository.save(handbook)
+            return 'ok'
+        } catch (e) {
+            log.error(e.message, e)
+            return 'ErrorPage'
+        }
+        return 'ok'
+    }
+
+    @PutMapping("api/handbook/{name}")
+    def update(@PathVariable String name, @RequestBody Handbook handbook) {
+        log.debug("Start update handbook by name $name")
+        try {
+            def doc = handbookRepository.findByName(name)
+            handbook.id = doc.id
+            handbookRepository.save(handbook)
+            return 'ok'
+        } catch (e) {
+            log.error(e.message, e)
+            return 'ErrorPage'
         }
     }
 }
